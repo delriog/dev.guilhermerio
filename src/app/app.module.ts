c047b93ab75be319566f29bb644bb0eaa59a63c6
Guilherme <guilhermebdelrio@gmail.com>
2023-08-01 15:50:10 -0300

    Adding navbar

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,8 +4,11 @@ import { BrowserModule } from '@angular/platform-browser';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { MenuHeaderComponent } from './menu-header/menu-header.component';
+
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 import { MatIconModule } from '@angular/material/icon';
+import { HttpClient, HttpClientModule } from '@angular/common/http';
+
 
 @NgModule({
   declarations: [
@@ -17,6 +20,7 @@ import { MatIconModule } from '@angular/material/icon';
     AppRoutingModule,
     BrowserAnimationsModule,
     MatIconModule,
+    HttpClientModule
   ],
   providers: [],
   bootstrap: [AppComponent]
--- src/app/menu-header/menu-header.component.ts
+++ src/app/menu-header/menu-header.component.ts
@@ -1,10 +1,36 @@
 import { Component } from '@angular/core';
+import { MatIconRegistry } from '@angular/material/icon';
+import { DomSanitizer } from '@angular/platform-browser';
 
 @Component({
   selector: 'app-menu-header',
   templateUrl: './menu-header.component.html',
-  styleUrls: ['./menu-header.component.css']
+  styleUrls: ['./menu-header.component.css'],
 })
 export class MenuHeaderComponent {
-
+  constructor(
+    private matIconRegistry: MatIconRegistry,
+    private domSanitizer: DomSanitizer
+  ) {
+    this.matIconRegistry.addSvgIcon(
+      'linkedin',
+      this.domSanitizer.bypassSecurityTrustResourceUrl('../assets/svg/linkedin.svg')
+    )
+    this.matIconRegistry.addSvgIcon(
+      'github',
+      this.domSanitizer.bypassSecurityTrustResourceUrl('../assets/svg/github.svg')
+    )
+    this.matIconRegistry.addSvgIcon(
+      'whatsapp',
+      this.domSanitizer.bypassSecurityTrustResourceUrl('../assets/svg/whatsapp.svg')
+    )
+    this.matIconRegistry.addSvgIcon(
+      'email',
+      this.domSanitizer.bypassSecurityTrustResourceUrl('../assets/svg/email.svg')
+    )
+    this.matIconRegistry.addSvgIcon(
+      'dev',
+      this.domSanitizer.bypassSecurityTrustResourceUrl('../assets/svg/dev-svgrepo-com.svg')
+    )
+  }
 }
